72118f74f6041ec5938dd2029c116b5beaa02687
Faisal Ali <fali@IRFALI123.local>
2016-02-18 08:36:36 +0000

    deleted / moved installscripts to its own reprository

--- gpdbinstallscripts/installscript_path.sh
+++
@@ -1,6 +0,0 @@
-export scriptpath=/usr/local/gpdbinstallscripts/
-export bin_dir=$scriptpath/bin
-INSTALLSCRIPTHOME=$bin_dir
-PATH=$INSTALLSCRIPTHOME:$PATH
-export displaymsg=1
-# cat $scriptpath/README > &2
